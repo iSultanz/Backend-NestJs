b0b4bb263724fc2ff5b09abc74ceee1df2050fcc
iSultanz <mrfalcooon@gmail.com>
2022-06-28 12:24:03 +0300

    ormconfig change

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,32 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRootAsync({
-//       imports: [ConfigModule],
-//       inject: [ConfigService],
-//       useFactory: (configService: ConfigService) => ({
-//         type: 'postgres',
-//         host: configService.get('DB_HOST'),
-//         port: configService.get('DB_PORT'),
-//         username: configService.get('DB_USERNAME'),
-//         password: configService.get('DB_PASSWORD'),
-//         database: configService.get('DB_DATABASE'),
-//         entities: ['./*.entity{.ts,.js}'],
-//         autoLoadEntities: true,
-//         synchronize: false,
-//         migrations: ["dist/src/db/migrations/**/*{.js,.ts}"],
-//         cli: {
-//           migrationsDir: 'src/db/migrations'
-//         },
-//       }),
-//     }),
-//   ],
-// })
-// export class DatabaseModule {}
-
 import * as dotenv from 'dotenv';
 dotenv.config();
 export = {
